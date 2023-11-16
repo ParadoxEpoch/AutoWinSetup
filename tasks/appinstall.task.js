6e3d1f42706ed84e257adccb4af6ef8375c2794c
Tristan Gauci <tristan@maega.com.au>
2023-11-16 12:15:09 +1100

    Add support for downloading apps via http

--- tasks/appinstall.task.js
+++ tasks/appinstall.task.js
@@ -1,110 +1,150 @@
 import inquirer from "inquirer";
 import { execute, printLogo, msg } from "../common.js";
+import axios from 'axios';
+import fs from 'fs';
+import tmp from 'tmp';
+import progress from 'progress-stream';
+import ProgressBar from 'cli-progress';
+
+tmp.setGracefulCleanup();
 
 const appList = [
     {
         id: "7zip.7zip",
-        name: "7zip"
+        name: "7zip",
+        source: "winget"
     },
     {
         id: "Valve.Steam",
-        name: "Steam"
+        name: "Steam",
+        source: "winget"
     },
     {
         id: "voidtools.Everything",
-        name: "Everything"
+        name: "Everything",
+        source: "winget"
     },
     {
         id: "AntibodySoftware.WizTree",
-        name: "WizTree"
+        name: "WizTree",
+        source: "winget"
     },
     {
         id: "GitHub.GitHubDesktop",
-        name: "GitHub Desktop"
+        name: "GitHub Desktop",
+        source: "winget"
     },
     {
         id: "Microsoft.VisualStudioCode",
-        name: "Visual Studio Code"
+        name: "Visual Studio Code",
+        source: "winget"
     },
     {
         id: "Spotify.Spotify",
-        name: "Spotify"
+        name: "Spotify",
+        source: "winget"
     },
     {
         id: "AgileBits.1Password",
-        name: "1Password"
+        name: "1Password",
+        source: "winget"
     },
     {
         id: "VideoLAN.VLC",
-        name: "VLC"
+        name: "VLC",
+        source: "winget"
     },
     {
         id: "Plex.Plex",
-        name: "Plex"
+        name: "Plex",
+        source: "winget"
     },
     {
         id: "PowerSoftware.AnyBurn",
-        name: "AnyBurn"
+        name: "AnyBurn",
+        source: "winget"
     },
     {
         id: "HulubuluSoftware.AdvancedRenamer",
-        name: "Advanced Renamer"
+        name: "Advanced Renamer",
+        source: "winget"
     },
     {
         id: "Discord.Discord",
-        name: "Discord"
+        name: "Discord",
+        source: "winget"
     },
     {
         id: "MHNexus.HxD",
-        name: "HxD"
+        name: "HxD",
+        source: "winget"
     },
     {
         id: "AppWork.JDownloader",
-        name: "JDownloader"
+        name: "JDownloader",
+        source: "winget"
     },
     {
         id: "Postman.Postman",
-        name: "Postman"
+        name: "Postman",
+        source: "winget"
     },
     {
         id: "PrivateInternetAccess.PrivateInternetAccess",
-        name: "Private Internet Access"
+        name: "Private Internet Access",
+        source: "winget"
     },
     {
         id: "PlayStation.PSRemotePlay",
-        name: "PS Remote Play"
+        name: "PS Remote Play",
+        source: "winget"
     },
     {
         id: "qBittorrent.qBittorrent",
-        name: "qBittorrent"
+        name: "qBittorrent",
+        source: "winget"
     },
     {
         id: "TablePlus.TablePlus",
-        name: "TablePlus"
+        name: "TablePlus",
+        source: "winget"
     },
     {
         id: "CodeSector.TeraCopy",
-        name: "TeraCopy"
+        name: "TeraCopy",
+        source: "winget"
     },
     {
         id: "StartIsBack.StartAllBack",
-        name: "StartAllBack"
+        name: "StartAllBack",
+        source: "winget"
     },
     {
         id: "eliboa.TegraRcmGUI",
-        name: "TegraRcmGUI"
+        name: "TegraRcmGUI",
+        source: "winget"
     },
     {
         id: "Notion.Notion",
-        name: "Notion"
+        name: "Notion",
+        source: "winget"
     },
     {
         id: "VMware.WorkstationPro",
-        name: "VMware Workstation Pro"
+        name: "VMware Workstation Pro",
+        source: "winget"
     },
     {
         id: "Microsoft.PowerToys",
-        name: "PowerToys"
+        name: "PowerToys",
+        source: "winget"
+    },
+    {
+        id: "RockstarGamesLauncher",
+        url: "https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe",
+        name: "Rockstar Games Launcher",
+        launchArgs: "",
+        source: "http"
     }
 ]
 
@@ -117,7 +157,7 @@ export default async function main() {
         choices.push({
             name: app.name,
             value: app.id,
-            checked: true
+            checked: false
         });
     }
 
@@ -132,17 +172,76 @@ export default async function main() {
 
     printLogo('Installing apps...');
 
-    for (const app of answer.apps) {
-        const appName = appList.find(x => x.id === app).name;
+    for (const appId of answer.apps) {
+        const app = appList.find(x => x.id === appId);
+        console.log(msg.info(`Installing ${app.name}...`));
         try {
-            console.log(msg.info(`Installing ${appName}...`));
-            await execute(`winget install -e --id ${app}`);
+            if (app.source === "winget") {
+                await execute(`winget install -e --id ${app.id}`);
+            } else if (app.source === "http") {
+                const tmpFile = await downloadBinary(app.url);
+                await execute(`"${tmpFile}" ${app.launchArgs || ''}`);
+            } else {
+                console.log(msg.error(`✗ Unknown source for ${app.name}\n`));
+            }
+            
             console.log(msg.success(`✓ Installation complete!\n`));
         } catch (e) {
-            console.log(msg.error(`✗ Error installing ${appName}\n`));
+            console.log(msg.error(`✗ Error installing ${app.name}\n`));
         }
     }
 
     console.log(msg.success("Software installation complete!"));
     return true;
 }
+
+const downloadBinary = async (url) => {
+    try {
+        const response = await axios.get(url, {
+            responseType: 'stream',
+            maxRedirects: 5,
+        });
+
+        const tmpFile = tmp.tmpNameSync({ postfix: '.exe' });
+
+        const writer = fs.createWriteStream(tmpFile);
+
+        var str;
+        if (response.headers['content-length'] !== undefined) {
+
+            const progressBar = new ProgressBar.SingleBar({}, ProgressBar.Presets.shades_classic);
+            progressBar.start(parseInt(response.headers['content-length']), 0);
+
+            str = progress({
+                length: response.headers['content-length'],
+                time: 100,
+            });
+
+            str.on('progress', function (progress) {
+                progressBar.update(progress.transferred);
+            });
+
+            response.data.pipe(str).pipe(writer);
+
+            await new Promise((resolve, reject) => {
+                writer.on('finish', () => {
+                    progressBar.stop();
+                    resolve(tmpFile);
+                });
+                writer.on('error', reject);
+            });
+        } else {
+            console.log(msg.bold("Downloading, please wait..."));
+            response.data.pipe(writer);
+
+            await new Promise((resolve, reject) => {
+                writer.on('finish', () => resolve(tmpFile));
+                writer.on('error', reject);
+            });
+        }
+        return tmpFile;
+    } catch (err) {
+        console.log(msg.error('Error downloading file'));
+        console.error(err);
+    }
+};
