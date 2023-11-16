2da8405642bb240dfa3a1114765bbb864ebb13c4
Tristan Gauci <tristan@maega.com.au>
2023-11-16 15:20:49 +1100

    Replace downloader function with @paradoxepoch/node-file-downloader

--- tasks/appinstall.task.js
+++ tasks/appinstall.task.js
@@ -1,10 +1,7 @@
 import inquirer from "inquirer";
 import { execute, printLogo, msg } from "../common.js";
-import axios from 'axios';
-import fs from 'fs';
 import tmp from 'tmp';
-import progress from 'progress-stream';
-import ProgressBar from 'cli-progress';
+import downloadBinary from "@paradoxepoch/node-file-downloader";
 
 tmp.setGracefulCleanup();
 
@@ -179,7 +176,10 @@ export default async function main() {
             if (app.source === "winget") {
                 await execute(`winget install -e --id ${app.id}`);
             } else if (app.source === "http") {
-                const tmpFile = await downloadBinary(app.url);
+                const tmpFile = await downloadBinary(app.url, null, {
+                    downloadMsg: null,
+                    successMsg: null
+                });
                 await execute(`"${tmpFile}" ${app.launchArgs || ''}`);
             } else {
                 console.log(msg.error(`✗ Unknown source for ${app.name}\n`));
@@ -194,54 +194,3 @@ export default async function main() {
     console.log(msg.success("Software installation complete!"));
     return true;
 }
-
-const downloadBinary = async (url) => {
-    try {
-        const response = await axios.get(url, {
-            responseType: 'stream',
-            maxRedirects: 5,
-        });
-
-        const tmpFile = tmp.tmpNameSync({ postfix: '.exe' });
-
-        const writer = fs.createWriteStream(tmpFile);
-
-        var str;
-        if (response.headers['content-length'] !== undefined) {
-
-            const progressBar = new ProgressBar.SingleBar({}, ProgressBar.Presets.shades_classic);
-            progressBar.start(parseInt(response.headers['content-length']), 0);
-
-            str = progress({
-                length: response.headers['content-length'],
-                time: 100,
-            });
-
-            str.on('progress', function (progress) {
-                progressBar.update(progress.transferred);
-            });
-
-            response.data.pipe(str).pipe(writer);
-
-            await new Promise((resolve, reject) => {
-                writer.on('finish', () => {
-                    progressBar.stop();
-                    resolve(tmpFile);
-                });
-                writer.on('error', reject);
-            });
-        } else {
-            console.log(msg.bold("Downloading, please wait..."));
-            response.data.pipe(writer);
-
-            await new Promise((resolve, reject) => {
-                writer.on('finish', () => resolve(tmpFile));
-                writer.on('error', reject);
-            });
-        }
-        return tmpFile;
-    } catch (err) {
-        console.log(msg.error('Error downloading file'));
-        console.error(err);
-    }
-};
