fe3a115a538f72b8efdf3d06f52f3c7d41b1851e
Tristan Gauci <tristan@maega.com.au>
2023-12-04 15:17:02 +1100

    AutoWin can now be restarted after completing tasks

--- main.js
+++ main.js
@@ -3,41 +3,61 @@ import inquirer from 'inquirer';
 import { printLogo, msg, sleep } from './common.js';
 import tasks from './tasks.js';
 
-printLogo('Select the tasks you want me to perform');
-
-const answer = await inquirer.prompt({
-    type: 'checkbox',
-    message: 'Select tasks',
-    name: 'tasks',
-    pageSize: 20,
-    choices: [
-        new inquirer.Separator('----- Software Installation -----'),
-        {name: 'Remove Windows Bloatware', value: 'debloat', checked: true},
-        {name: 'Install Common Apps', value: 'appInstall', checked: true},
-        new inquirer.Separator('----- Explorer Tweaks -----'),
-        {name: 'Disable Windows 11 (WinUI) Context Menu', value: 'disableWin11CxtMenu', checked: true},
-        {name: 'Set File Explorer to open to This PC', value: 'explorerOpenToThisPC', checked: true},
-        {name: 'Show File Extensions in File Explorer', value: 'explorerShowExtensions', checked: true},
-        {name: 'Hide Home tab in File Explorer', value: 'explorerDisableHomeTab', checked: true},
-        new inquirer.Separator('----- Other Tasks -----'),
-        {name: 'Enable ssh-agent', value: 'enableSshAgent', checked: true},
-        //{name: 'Install NVIDIA Graphics Driver', value: 'nvinstall', checked: false},
-    ],
-    validate: function(answer) {
-        return answer.length < 1 ? 'So... we\'re doing nothing? Pick something pls.' : true;
+async function main() {
+    printLogo('Select the tasks you want me to perform');
+    const answer = await inquirer.prompt({
+        type: 'checkbox',
+        message: 'Select tasks',
+        name: 'tasks',
+        pageSize: 20,
+        choices: [
+            new inquirer.Separator('----- Software Installation -----'),
+            {name: 'Remove Windows Bloatware', value: 'debloat', checked: true},
+            {name: 'Install Common Apps', value: 'appInstall', checked: true},
+            new inquirer.Separator('----- Explorer Tweaks -----'),
+            {name: 'Disable Windows 11 (WinUI) Context Menu', value: 'disableWin11CxtMenu', checked: true},
+            {name: 'Set File Explorer to open to This PC', value: 'explorerOpenToThisPC', checked: true},
+            {name: 'Show File Extensions in File Explorer', value: 'explorerShowExtensions', checked: true},
+            {name: 'Hide Home tab in File Explorer', value: 'explorerDisableHomeTab', checked: true},
+            {name: 'Disable "Previous Versions" and remove context menu item', value: 'explorerDisablePreviousVersions', checked: true},
+            {name: 'Delete Superfluous Folders in User Profile', value: 'deleteExtraProfileDirs', checked: true},
+            new inquirer.Separator('----- Remove Context Menu Items -----'),
+            {name: 'Remove "Give Access To..."', value: 'explorerDisableCxtGiveAccessTo', checked: true},
+            {name: 'Remove "Scan with Microsoft Defender..."', value: 'explorerDisableCxtScanWithDefender', checked: true},
+            {name: 'Remove "Add to Favorites"', value: 'explorerDisableCxtAddToFavorites', checked: true},
+            {name: 'Remove "Share"', value: 'explorerDisableCxtShare', checked: true},
+            new inquirer.Separator('----- Other Tasks -----'),
+            {name: 'Enable Windows Sandbox', value: 'enableSandboxFeature', checked: true},
+            {name: 'Enable ssh-agent', value: 'enableSshAgent', checked: true}
+        ],
+        validate: function(answer) {
+            return answer.length < 1 ? 'So... we\'re doing nothing? Pick something pls.' : true;
+        }
+    });
+    
+    printLogo();
+    
+    const taskCompleted = {};
+    
+    for (const task of answer.tasks) {
+        taskCompleted[task] = false;
+        const result = await tasks[task]();
+        taskCompleted[task] = result;
+        await sleep(1000);
     }
-});
 
-printLogo();
+    console.log(msg.success('==> All tasks completed!\n'));
 
-const taskCompleted = {};
+    const restart = await inquirer.prompt({
+        type: 'confirm',
+        name: 'do',
+        message: 'Would you like to do something else?',
+        default: true,
+    });
 
-for (const task of answer.tasks) {
-    taskCompleted[task] = false;
-    const result = await tasks[task]();
-    taskCompleted[task] = result;
+    restart.do
+        ? main(false)
+        : printLogo('Thank you, come again!') && process.exit(0);
 }
 
-console.log(msg.success('\nAll tasks completed! Exiting in 5 seconds...'));
-await sleep(5000);
-process.exit(0);
+main();
