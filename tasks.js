b2f6ec5f184b408744a842f5ea50be164ff8e70e
Tristan Gauci <tristan@maega.com.au>
2024-02-15 16:25:23 +1100

    Added several new windows tweaks

--- tasks.js
+++ tasks.js
@@ -13,6 +13,22 @@ const tasks = {
 		await executeNoFail(`reg add "HKLM\\Software\\Policies\\Microsoft\\Windows\\CloudContent" /v "DisableWindowsConsumerFeatures" /t REG_DWORD /d 1 /f`, 'Setting DisableWindowsConsumerFeatures DWORD to 1...');
 		return true;
 	},
+	disableFastStartup: async () => {
+		console.log(msg.info(`==> Disabling Fast Startup...\n`));
+		await executeNoFail(`reg add "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Power" /v "HiberbootEnabled" /t REG_DWORD /d 0 /f`, 'Setting HiberbootEnabled DWORD to 0...');
+		return true;
+	},
+	removeDuplicateDrives: async () => {
+		console.log(msg.info(`==> Removing duplicate drives from "This PC"...\n`));
+		await executeNoFail(`reg delete "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\DelegateFolders\\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}" /f`, 'Removing registry key in DelegateFolders...');
+		await executeNoFail(`reg delete "HKLM\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\DelegateFolders\\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}" /f`, 'Removing registry key in WoW64 DelegateFolders...');
+		return true;
+	},
+	disableGallery: async () => {
+		console.log(msg.info(`==> Disabling Gallery in File Explorer...\n`));
+		await executeNoFail(`reg delete "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace_41040327\\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}" /f`, 'Removing registry key...');
+		return true;
+	},
 	disableWin11CxtMenu: async () => {
 		console.log(msg.info(`==> Disabling Windows 11 context menu...\n`));
 		await executeNoFail(`reg add "HKCU\\Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\\InProcServer32" /f /ve`, 'Adding registry key...');
@@ -125,6 +141,59 @@ const tasks = {
 		await executeNoFail(`reg delete "HKCR\\Drive\\shellex\\ContextMenuHandlers\\Sharing" /f`, 'Removing ContextMenuHandler from HKCR\\Drive key...');
 		await executeNoFail(`reg delete "HKCR\\LibraryFolder\\Background\\shellex\\ContextMenuHandlers\\Sharing" /f`, 'Removing ContextMenuHandler from HKCR\\LibraryFolder\\Background key...');
 		await executeNoFail(`reg delete "HKCR\\UserLibraryFolder\\shellex\\ContextMenuHandlers\\Sharing" /f`, 'Removing ContextMenuHandler from HKCR\\UserLibraryFolder key...');
+		return true;
+	},
+	explorerDisableCtxCastToDevice: async () => {
+		console.log(msg.info(`==> Removing "Cast to Device" context menu item...\n`));
+		await executeNoFail(`reg add "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Shell Extensions\\Blocked" /v "{7AD84985-87B4-4a16-BE58-8B72A5B390F7}" /t REG_SZ /d "" /f`, 'Adding to shellex blocklist in registry...');
+		return true;
+	},
+	explorerDisableCtxPrint: async () => {
+		console.log(msg.info(`==> Removing "Print" context menu item...\n`));
+		await executeNoFail(`reg add "HKCR\\SystemFileAssociations\\image\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for image files...');
+		await executeNoFail(`reg add "HKCR\\batfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for batch files...');
+		await executeNoFail(`reg add "HKCR\\cmdfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for cmd files...');
+		await executeNoFail(`reg add "HKCR\\docxfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for docx files...');
+		await executeNoFail(`reg add "HKCR\\fonfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for fon files...');
+		await executeNoFail(`reg add "HKCR\\htmlfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for html files...');
+		await executeNoFail(`reg add "HKCR\\inffile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for inf files...');
+		await executeNoFail(`reg add "HKCR\\inifile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for ini files...');
+		await executeNoFail(`reg add "HKCR\\JSEFile\\Shell\\Print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for JSE files...');
+		await executeNoFail(`reg add "HKCR\\otffile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for otf files...');
+		await executeNoFail(`reg add "HKCR\\pfmfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for pfm files...');
+		await executeNoFail(`reg add "HKCR\\regfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for reg files...');
+		await executeNoFail(`reg add "HKCR\\rtffile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for rtf files...');
+		await executeNoFail(`reg add "HKCR\\ttcfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for ttc files...');
+		await executeNoFail(`reg add "HKCR\\ttffile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for ttf files...');
+		await executeNoFail(`reg add "HKCR\\txtfile\\shell\\print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for txt files...');
+		await executeNoFail(`reg add "HKCR\\VBEFile\\Shell\\Print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for VBE files...');
+		await executeNoFail(`reg add "HKCR\\VBSFile\\Shell\\Print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for VBS files...');
+		await executeNoFail(`reg add "HKCR\\WSFFile\\Shell\\Print" /v "ProgrammaticAccessOnly" /t REG_SZ /d "" /f`, 'Adding ProgrammaticAccessOnly value to print key for WSF files...');
+		return true;
+	},
+	explorerDisableCtxImageRotation: async () => {
+		console.log(msg.info(`==> Removing "Rotate left" and "Rotate right" context menu items for images...\n`));
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.avci\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.avci key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.avif\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.avif key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.bmp\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.bmp key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.dds\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.dds key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.dib\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.dib key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.gif\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.gif key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.heic\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.heic key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.heif\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.heif key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.hif\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.hif key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.ico\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.ico key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.jfif\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.jfif key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.jpe\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.jpe key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.jpeg\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.jpeg key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.jpg\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.jpg key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.jxr\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.jxr key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.png\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.png key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.rle\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.rle key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.tif\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.tif key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.tiff\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.tiff key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.wdp\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.wdp key...');
+		await executeNoFail(`reg delete "HKCR\\SystemFileAssociations\\.webp\\ShellEx\\ContextMenuHandlers\\ShellImagePreview" /f`, 'Removing ShellImagePreview from HKCR\\SystemFileAssociations\\.webp key...');
 		return true;
 	},
 	explorerDisablePreviousVersions: async () => {
